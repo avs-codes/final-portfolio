809726f6a009b03630519f613832f5fb09123d5e
avs-codes <90225704+avs-codes@users.noreply.github.com>
2021-12-07 17:16:07 -0500

    Import from Glitch
    Code copied from Glitch page and put here

---
+++ scripts/spCode.js
@@ -0,0 +1,14 @@
+export function spCode()  {
+    let noiseScale = 5.2;
+   
+   let s = getSpace();
+   let n = noise(s * noiseScale + time*.2 + .01);
+   n = nsin(n * 7);
+   
+   let nColor = pow(n, 3);
+   let col = vec3(nColor+.2, .3, nColor+2) + normal * .4;
+   color(col);
+   
+   sphere(0.5 + n*.02);
+     
+   };
